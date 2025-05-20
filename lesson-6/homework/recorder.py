0860f6e7fdcfa3da4915b186b517d33718c141ea
adwertyn <nosirjonov186@gmail.com>
2025-05-20 18:02:17 +0500

    lesson-6

--- lesson-6/homework/recorder.py
+++ lesson-6/homework/recorder.py
@@ -1,49 +1,69 @@
-
-
 def add_employee():
     while True:
-        try:
-            employee_id = input("Enter Employee ID: ")
+        employee_id = input("Enter Employee ID: ")
+        if employee_id.isdigit():
             break
-        except ValueError:
-            print("You can't enter string. Please enter a number!")
-            continue
+        else:
+            print("Please enter a valid numeric Employee ID.")
 
     while True:
-        name = input("Enter a name: ")
+        name = input("Enter Name: ")
         if name.isdigit():
             print("Enter a string, not a number!")
-            continue
         else:
             break
+
     while True:
-        position = input("Enter a Position: ")
+        position = input("Enter Position: ")
         if position.isdigit():
             print("Enter a string, not a number!")
-            continue
         else:
             break
-    
+
+    salary = input("Enter Salary: ")
+
+    with open("employees.txt", "a") as file:
+        file.write(f"{employee_id}, {name}, {position}, {salary}\n")
+    print("Employee added successfully!\n")
+
+def display_records():
+    with open("employees.txt", 'r') as file:
+        records = file.readlines()
+        if not records:
+            print("No employee records")
+        else:
+            print("Employee Records:")
+            for record in records:
+                print(record.strip())
+def search_info():
+    employee_id = input("Enter Employee ID to search: ")
+    with open("employees.txt", "r") as file:
+        for line in file:
+            if line.startswith(employee_id + ","):
+                print("Employee Found: ", line.strip())
+                found = True
+                break
+    if not found:
+        print("Employee not found.\n")
 
 
-employees_file = open("employees.txt", mode='wt')
-
-print('''1. Add new employee record
-2. View all employee records
-3. Search for an employee by Employee ID
-4. Update an employee's information
-5. Delete an employee record
-6. Exit''')
-option = int(input("Choose one option! Enter the number of option: "))
-if option == 1:
-    add_employee()
-elif option == 2:
-    display_records()
-elif option == 3:
-    search_info()
-elif option == 4:
-    update_record()
-elif option == 5:
-    delete_record()
-else:
-    exit()
+while True:
+    print('''1. Add new employee record
+    2. View all employee records
+    3. Search for an employee by Employee ID
+    4. Update an employee's information
+    5. Delete an employee record
+    6. Exit''')
+    option = int(input("Choose one option! Enter the number of option: "))
+    if option == 1:
+        add_employee()
+    elif option == 2:
+        display_records()
+    elif option == 3:
+        search_info()
+    elif option == 4:
+        update_record()
+    elif option == 5:
+        delete_record()
+    else:
+        exit()
